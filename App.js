2b64e623838a709913c48a715c7d9ebb2d6ff26e
Spencer Tassone <spencerwilliamtassone@gmail.com>
2020-02-18 00:41:13 -0500

    added header component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -10,7 +11,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* Header */}
+      <Header />
+      
       <View style={styles.content}>
         {/* to from */}
 
@@ -38,6 +40,6 @@ const styles = StyleSheet.create({
   },
   list: {
     marginTop: 20,
-  }
+  },
 
 });
